d90bbdeafcf239366a371a9db1f7e73beeacc981
RobGro <r.grodzki74@gmail.com>
2021-05-24 18:23:19 +0100

    init chuck jokes app

---
+++ src/main/java/com/robgro/chuckjokes/ChuckJokesApplication.java
@@ -0,0 +1,13 @@
+package com.robgro.chuckjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckJokesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robgro/chuckjokes/ChuckJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robgro.chuckjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
